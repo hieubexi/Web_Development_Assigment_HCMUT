3d9ea18c82032e789c7a5a449379133d99bf5e3e
hieubexi <hieu.nguyen301202@gmail.com>
2024-03-28 09:57:36 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,19 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Your Page Title</title>
+    <!-- Include any CSS files if needed -->
+</head>
+<body>
 <?php
 include("connection/connect.php");
 error_reporting(0);
 session_start();
 require_once("home.php")
 ?>
+
+</body>
+</html>
